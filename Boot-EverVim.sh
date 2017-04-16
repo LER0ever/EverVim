cb806951b97fcddf1612e3a2506e30a4a326ccd0
LER0ever <l.e.r@riseup.net>
2017-04-16 13:00:24 -0500

    feat(EverVimRC): copy configuration sample if .EverVim.vimrc does not exists

--- Boot-EverVim.sh
+++ Boot-EverVim.sh
@@ -3,7 +3,7 @@
 echo 'Welcome to EverVim, a powerful & modern vim distribution'
 echo 'Booting EverVim ...'
 
-if [ -d "~/.EverVim" ]; then
+if [ ! -d "~/.EverVim" ]; then
 	echo "Cloning into ~/.EverVim ..."
 	git clone https://github.com/LER0ever/EverVim ~/.EverVim
 	cd ~/.EverVim
@@ -16,6 +16,11 @@ echo 'Soft-Linking Vim/NeoVim Config ...'
 ln -s ~/.EverVim ~/.vim
 ln -s ~/.EverVim/vimrc ~/.EverVim/init.vim
 ln -s ~/.EverVim/core/gui.vim ~/.EverVim/ginit.vim
+
+if [ ! -f "~/.EverVim.vimrc" ]; then
+    echo "Copied EverVim configuration sample to ~/.EverVim.vimrc"
+    cp ~/.EverVim/.EverVim.vimrc.sample ~/.EverVim.vimrc
+fi
 
 echo 'Downloading Vim-Plug'
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
