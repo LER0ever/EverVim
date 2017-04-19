7cedde873a2bed8c34b7f2d165e98fb0e18b9036
LER0ever <etasry@gmail.com>
2017-04-19 00:44:46 -0500

    fix(setup): fix potential failure to create symlink

--- Boot-EverVim.sh
+++ Boot-EverVim.sh
@@ -3,22 +3,23 @@
 echo 'Welcome to EverVim, a powerful & modern vim distribution'
 echo 'Booting EverVim ...'
 
-if [ ! -d "~/.EverVim" ]; then
+cd ~
+
+if [ ! -d ".EverVim" ]; then
     echo "Cloning into ~/.EverVim ..."
     git clone https://github.com/LER0ever/EverVim ~/.EverVim
-    cd ~/.EverVim
 fi
 
 # Ensure we have .config
 mkdir -p ~/.config/
 
 echo 'Soft-Linking Vim/NeoVim Config ...'
-ln -s ~/.EverVim ~/.vim
-ln -s ~/.EverVim ~/.config/nvim
-ln -s ~/.EverVim/vimrc ~/.EverVim/init.vim
-ln -s ~/.EverVim/core/gui.vim ~/.EverVim/ginit.vim
+ln -sfnv ~/.EverVim ~/.vim
+ln -sfnv ~/.EverVim ~/.config/nvim
+ln -sfnv ~/.EverVim/vimrc ~/.EverVim/init.vim
+ln -sfnv ~/.EverVim/core/gui.vim ~/.EverVim/ginit.vim
 
-if [ ! -f "~/.EverVim.vimrc" ]; then
+if [ ! -f ".EverVim.vimrc" ]; then
     echo "Copied EverVim configuration sample to ~/.EverVim.vimrc"
     cp ~/.EverVim/.EverVim.vimrc.sample ~/.EverVim.vimrc
 fi
@@ -27,7 +28,9 @@ echo 'Downloading Vim-Plug'
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo 'Configuring CTags ...'
-ln -s ~/.EverVim/tools/ctags/ctags ~/.ctags
+if [ ! -f ".ctags" ]; then
+    echo 'Configuring CTags ...'
+    ln -sfnv ~/.EverVim/tools/ctags/ctags ~/.ctags
+fi
 
 echo 'All done with this script, now run vim/neovim and execute ":PlugInstall"'
